af631133bfde82432ddcfc3fba4ecd268fd64f7e
Diego Isaac Hurtado Miguel <108156269+diegoisaacpy@users.noreply.github.com>
2023-04-08 12:25:32 -0600

    Fist Program

---
+++ HelloWorld/src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.print("\nhola mi pana");
+        }
+    }
+}
