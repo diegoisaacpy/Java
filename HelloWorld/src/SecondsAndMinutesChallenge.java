3918ff50cd9f708009eee18d292a970fa788d781
Diego Isaac Hurtado Miguel <108156269+diegoisaacpy@users.noreply.github.com>
2023-04-12 18:49:46 -0600

    Third program

---
+++ HelloWorld/src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,27 @@
+public class SecondsAndMinutesChallenge {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
+    }
+    public static String getDurationString (int seconds) {
+        int minutes = seconds / 60;
+        return getDurationString(minutes, seconds);
+
+    }
+
+    public static String getDurationString (int minutes, int seconds) {
+
+        int hours = minutes / 60;
+        System.out.println("hours = " + hours);
+
+        int remainingMinutes = minutes % 60;
+        System.out.println("minutes = " + minutes);
+        System.out.println("remainingMinutes = " + remainingMinutes);
+
+        int remainingSeconds = seconds % 60;
+        System.out.println("remainingSeconds = " + remainingSeconds);
+
+        return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
+
+    }
+}
